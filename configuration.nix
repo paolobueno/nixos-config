7605e42a93f2357252eaa6b59eb7635a431267da
Paolo Bueno <paolohaji@gmail.com>
2018-07-01 12:30:54 +0000

    Remove some comments

--- configuration.nix
+++ configuration.nix
@@ -28,53 +28,23 @@
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   wget vim
-  # ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.bash.enableCompletion = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-  networking = {
-    firewall.enable = true;
-    firewall.allowedTCPPorts = [3000 9418];
-    networkmanager.enable = true;
-    hostName = "paolo-nixos";
-  };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  # networking = {
+    # firewall.enable = true;
+    # firewall.allowedTCPPorts = [3000 9418];
+    # networkmanager.enable = true;
+    # hostName = "paolo-nixos";
+  # };
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.paolo = {
