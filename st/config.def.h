9ab80e39a7ac0ec93dd2431260d1f9fc6373398a
Paolo Bueno <paolohaji@gmail.com>
2018-08-07 17:04:17 +0200

    Change to roboto mono

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Roboto Mono for Powerline:size=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
